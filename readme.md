16f83c744d6f26363b0c227c84ba01f2251d747c
JLi2007 <LiJames9532@gmail.com>
2026-03-14 23:05:46 -0400

    feat: project setup

--- readme.md
+++ readme.md
@@ -1,1 +1,12 @@
-agent swarm
+use uv for python microservices
+
+uv add PACKAGE
+uv run python src/main.py
+
+
+
+
+
+testing/
+is an html testing file
+will make a dashboard with react in the future but no frontend for now
--- services/classifier/src/main.py
+++ services/classifier/src/main.py
@@ -1,5 +1,12 @@
+import pika
+
 def main():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host="localhost")
+    )
+    channel = connection.channel()
     print("waiting for messages...")
+    channel.start_consuming()
 
 if __name__ == "__main__":
     main()
